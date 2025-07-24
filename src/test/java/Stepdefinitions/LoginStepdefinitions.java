34d0acfe507a4bbbb3149b200faaed30b507e04b
praveensinghal2010 <praveen.singhal2010@gmail.com>
2025-07-24 22:27:01 +0530

    hello

--- src/test/java/Stepdefinitions/LoginStepdefinitions.java
+++ src/test/java/Stepdefinitions/LoginStepdefinitions.java
@@ -4,14 +4,23 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginStepdefinitions {
 
     WebDriver driver;
     @Given("User is homepage")
     public void user_is_homepage() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Headless mode
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*"); // Optional
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--user-data-dir=/tmp/unique-profile-" + System.currentTimeMillis());
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
 
